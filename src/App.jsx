d8d6da13899edd8c04dd64d20c1bbb60aaf1c9fb
AnuragDarji <anuragdarji29@gmail.com>
2024-06-15 17:47:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      {/* <Navbar title="textUtils2" aboutTitle="about text"/> */}
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
